f89d7b457f5190083e7b1a7078c1c2c64c61f626
deepa-ramadoss-tfs <58583783+deepa-ramadoss-tfs@users.noreply.github.com>
2022-08-10 13:03:27 +0530

    key event

--- jbrowse-plugin-my-project/src/keydown.js
+++ jbrowse-plugin-my-project/src/keydown.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import useKeyDown from './useKeyDown';
 
-const KeyDown = ({childToParent}) => {
+const KeyDown = ({childToParent, feature}) => {
   const [x, y] = useKeyDown();
   {childToParent([x,y])}
+  console.log("FEATURE ", feature);
   return (
     <h1>
       The key is ({x}, {y}): from plugin
+    
+      <div>{feature}</div>
     </h1>
   );
 };
